2a711368d82187d2d012b172be637f95f1070953
licheng <licheng@jk.cn>
2016-11-21 15:13:50 +0800

    mybatis mapper add

---
+++ src/main/java/com/lc/dr/mapper/TestMapper.java
@@ -0,0 +1,17 @@
+package com.lc.dr.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * Desc:
+ * Author:   licheng
+ * Datetime: 2016-11-21 11:35
+ */
+@Mapper
+public interface TestMapper {
+
+    @Insert("insert into users(description) values(#{desc})")
+    int insertTest(@Param("desc") String desc);
+}
---
+++ src/test/java/com/lc/dr/mapper/MapperTest.java
@@ -0,0 +1,27 @@
+package com.lc.dr.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * Desc:
+ * Author:   licheng
+ * Datetime: 2016-11-21 11:43
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MapperTest {
+
+    @Resource
+    TestMapper testMapper;
+
+    @Test
+    public void testMapper(){
+
+        System.out.println(testMapper.insertTest("lcdr"));
+    }
+}
