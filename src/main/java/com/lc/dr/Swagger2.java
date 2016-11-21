71d3a4d05ed04e1a570fb3730a749305440c652f
lc_dream_real <lc_dream_real@icloud.com>
2016-11-22 00:00:40 +0800

    swagger2 modify

--- src/main/java/com/lc/dr/Swagger2.java
+++ src/main/java/com/lc/dr/Swagger2.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -22,6 +23,7 @@ public class Swagger2 {
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("LC_DR_RESTFUL_API")
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.lc.dr.controller"))
@@ -33,8 +35,7 @@ public class Swagger2 {
         return new ApiInfoBuilder()
                 .title("Spring Boot中使用Swagger2构建RESTful APIs")
                 .description("github地址：https://github.com/LCDreamReal/LC_DR")
-                .termsOfServiceUrl("https://github.com/LCDreamReal/LC_DR")
-                .contact("LC_DR")
+                .contact(new Contact("LC_DR", "https://github.com/LCDreamReal/LC_DR", "1103429054@qq.com"))
                 .version("1.0")
                 .build();
     }
