9f575d892a1cbdd1cccf9100fd717a18794a99d8
lc_dream_real <lc_dream_real@icloud.com>
2017-06-08 07:07:11 +0800

    add spring-boot-actuator

--- src/main/java/com/lc/dr/controller/IndexController.java
+++ src/main/java/com/lc/dr/controller/IndexController.java
@@ -1,7 +1,8 @@
 package com.lc.dr.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import springfox.documentation.annotations.ApiIgnore;
@@ -14,10 +15,15 @@ import springfox.documentation.annotations.ApiIgnore;
 @RequestMapping(value = "")
 public class IndexController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(IndexController.class);
+
     @ApiIgnore
     @RequestMapping(value = "index")
     public String index(ModelMap model){
 
+        LOGGER.debug("debug level test");
+        LOGGER.info("info level test");
+        LOGGER.error("error level test");
         model.addAttribute("name", "Hello LC_DR");
         return "index";
     }
